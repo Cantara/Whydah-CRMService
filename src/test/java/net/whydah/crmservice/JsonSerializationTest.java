6f2614288fc5f71d41b8f2f6bcd68a440c210e6f
Thor Henning Hetland <totto@totto.org>
2016-03-07 19:12:15 +0100

    added more data to json domain object test to emphasis structural weakness

--- src/test/java/net/whydah/crmservice/JsonSerializationTest.java
+++ src/test/java/net/whydah/crmservice/JsonSerializationTest.java
@@ -3,10 +3,14 @@ package net.whydah.crmservice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.whydah.crmservice.customer.model.Customer;
 import net.whydah.crmservice.customer.model.DeliveryAddress;
+import net.whydah.crmservice.customer.model.EmailAddress;
+import net.whydah.crmservice.customer.model.PhoneNumber;
 import org.junit.Test;
+
 
 import java.util.HashMap;
 import java.util.Map;
+
 
 public class JsonSerializationTest {
 
@@ -14,7 +18,7 @@ public class JsonSerializationTest {
 
 
     @Test
-    public void createDummyCustomer() throws Exception {
+    public void createExampleCustomerAndCheckJson() throws Exception {
         Customer customer = new Customer();
         customer.setId("12345");
         customer.setFirstname("First");
@@ -27,9 +31,35 @@ public class JsonSerializationTest {
         addr1.setPostalcity("Oslo");
         addresses.put("work", addr1);
 
+        DeliveryAddress addr2 = new DeliveryAddress();
+        addr2.setAddressLine1("Møllefaret 30E");
+        addr2.setPostalcode("0750");
+        addr2.setPostalcity("Oslo");
+        addresses.put("home", addr2);
         customer.setDeliveryaddresses(addresses);
 
-        System.out.println(mapper.writeValueAsString(customer));
+        Map<String, EmailAddress> emailAddressMap = new HashMap<>();
+        EmailAddress email1 = new EmailAddress("totto@tott.org", "hjemme, privat, OID");
+        emailAddressMap.put("hjemme", email1);
+        EmailAddress email2 = new EmailAddress("totto@cantara.no", "opensource, privat, OID");
+        emailAddressMap.put("community", email2);
+        EmailAddress email3 = new EmailAddress("totto@capraconsulting.no", "jobb, Capra, OID");
+        emailAddressMap.put("jobb", email3);
+        EmailAddress email4 = new EmailAddress("thor.henning.hetland@nmd.no", "jobb, kunde, OID");
+        emailAddressMap.put("kobb-kunde", email4);
+        customer.setEmailaddresses(emailAddressMap);
+
+
+        Map<String, PhoneNumber> phoneNumberMap = new HashMap<>();
+        PhoneNumber p1 = new PhoneNumber("jobb", "91905054");
+        phoneNumberMap.put("tja", p1);
+        PhoneNumber p2 = new PhoneNumber("privat", "96909999");
+        phoneNumberMap.put("tja", p2);
+
+        customer.setPhonenumbers(phoneNumberMap);
+
+        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(customer));
+
     }
 
 }
