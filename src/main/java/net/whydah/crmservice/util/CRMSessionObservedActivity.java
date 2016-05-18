a13cfd5c213c324def29230bb8c6587816918221
totto <totto@totto.org>
2016-05-18 17:43:30 +0200

    added skeleton to obsreve CRM activities

---
+++ src/main/java/net/whydah/crmservice/util/CRMSessionObservedActivity.java
@@ -0,0 +1,15 @@
+package net.whydah.crmservice.util;
+
+import org.valuereporter.agent.activity.ObservedActivity;
+
+public class CRMSessionObservedActivity extends ObservedActivity {
+    public static final String USER_SESSION_ACTIVITY = "crmSession";
+    private static final String USER_SESSION_ACTIVITY_DB_KEY = "crmid";
+
+    public CRMSessionObservedActivity(String userid, String userSessionActivity, String applicationtokenid) {
+        super(USER_SESSION_ACTIVITY, System.currentTimeMillis());
+        put("crmid", userid);
+        put("crmsessionfunction", userSessionActivity);
+        put("applicationtokenid", applicationtokenid);
+    }
+}
