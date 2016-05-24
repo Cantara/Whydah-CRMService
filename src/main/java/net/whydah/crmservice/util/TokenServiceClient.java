549c58c87438a70b09b6372f84be460f95853c5a
totto <totto@totto.org>
2016-05-24 21:21:15 +0200

    Aligned with the latest BaseWhydahClient and sdk 2.1.271

--- src/main/java/net/whydah/crmservice/util/TokenServiceClient.java
+++ src/main/java/net/whydah/crmservice/util/TokenServiceClient.java
@@ -2,45 +2,25 @@ package net.whydah.crmservice.util;
 
 
 import com.google.inject.Singleton;
-import net.whydah.sso.commands.appauth.CommandValidateApplicationTokenId;
-import net.whydah.sso.commands.userauth.CommandGetUsertokenByUsertokenId;
-import net.whydah.sso.session.WhydahApplicationSession;
-import net.whydah.sso.util.SSLTool;
+import net.whydah.sso.session.baseclasses.BaseWhydahServiceClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 
 @Singleton
-public class TokenServiceClient {
+public class TokenServiceClient extends BaseWhydahServiceClient {
 
     private static final Logger log = LoggerFactory.getLogger(TokenServiceClient.class);
 
-    private final WhydahApplicationSession applicationSession;
 
     public TokenServiceClient(String securitytokenserviceurl,
                               String activeApplicationId,
                               String applicationname,
                               String applicationsecret) throws URISyntaxException {
 
-        log.warn("SSL disabled for development - should be removed.");
-        SSLTool.disableCertificateValidation();
-        applicationSession = WhydahApplicationSession.getInstance(securitytokenserviceurl, activeApplicationId, applicationname, applicationsecret);
+        super(securitytokenserviceurl, null, activeApplicationId, applicationname, applicationsecret);
     }
 
-    public Boolean isApplicationTokenIdValid(String applicationTokenId) {
-        return new CommandValidateApplicationTokenId(applicationSession.getSTS(), applicationTokenId).execute();
-    }
-
-    public String getUserTokenXml(String userTokenId) throws URISyntaxException {
-        log.warn("SSL disabled for development - should be removed.");
-        SSLTool.disableCertificateValidation();
-        return new CommandGetUsertokenByUsertokenId(new URI(applicationSession.getSTS()),
-                                                    applicationSession.getActiveApplicationTokenId(),
-                                                    applicationSession.getActiveApplicationTokenXML(),
-                                                    userTokenId).
-                                                    execute();
-    }
 
 }
