2c6845248420253da487e0d2cfabf94f444a58e7
Erik Drolshammer <erik-dev@fjas.no>
2019-02-12 11:51:08 +0100

    add debug logging to customer data not found in GetCustomerHandler.

--- src/main/java/net/whydah/crmservice/customer/GetCustomerHandler.java
+++ src/main/java/net/whydah/crmservice/customer/GetCustomerHandler.java
@@ -13,7 +13,6 @@ import static ratpack.jackson.Jackson.json;
 
 @Singleton
 public class GetCustomerHandler implements Handler {
-
     private static final Logger log = LoggerFactory.getLogger(GetCustomerHandler.class);
 
     private final CustomerRepository customerRepository;
@@ -22,26 +21,27 @@ public class GetCustomerHandler implements Handler {
     public GetCustomerHandler(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
+
     @Override
     public void handle(Context ctx) throws Exception {
-
         String customerRef = ctx.getPathTokens().get("customerRef");
         
         if (Authentication.isAdminUser()) {
-        	
+
         } else if (customerRef == null || !customerRef.equals(Authentication.getAuthenticatedUser().getPersonRef())) {
-            log.debug("User {} with personRef {} not authorized to get data for personRef {}", Authentication.getAuthenticatedUser().getUid(), Authentication.getAuthenticatedUser().getPersonRef(), customerRef);
+            log.debug("User {} with personRef {} not authorized to get data for personRef {}. Returning 401", Authentication.getAuthenticatedUser().getUid(), Authentication.getAuthenticatedUser().getPersonRef(), customerRef);
             ctx.clientError(401);
             return;
         }
 
-        log.trace("Getting customer with ref={}", customerRef);
+        log.trace("Getting customer with customerRef={}", customerRef);
 
         Blocking.get(() -> customerRepository.getCustomer(customerRef)).then(customer -> {
             log.trace("Found customer-data: {}", customer);
             if (customer != null) {
                 ctx.render(json(customer));
             } else {
+                log.debug("Did not find any customer data for customerRef={}. Returning 404", customerRef);
                 ctx.clientError(404); //Not found
             }
         });
